53dfbc332e87db281b8c29dc60e27b1ea41d4aed
Luis Carlos Charris Molina <luis.carlos.charris@gmail.com>
2024-02-19 09:01:27 -0500

    Init

---
+++ src/views/auth/logout/basic.vue
@@ -0,0 +1,83 @@
+<script>
+import animationData from "@/components/widgets/hzomhqxz.json";
+import Lottie from "@/components/widgets/lottie.vue";
+
+export default {
+  data() {
+    return {
+      defaultOptions: {
+        animationData: animationData
+      },
+    };
+  },
+  components: {
+    lottie: Lottie
+  },
+};
+</script>
+
+<template>
+  <div class="auth-page-wrapper pt-5">
+    <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+      <div class="bg-overlay"></div>
+
+      <div class="shape">
+
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 1440 120">
+          <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+        </svg>
+      </div>
+    </div>
+
+    <div class="auth-page-content">
+      <BContainer>
+        <BRow>
+          <BCol lg="12">
+            <div class="text-center mt-sm-5 mb-4 text-white-50">
+              <div>
+                <router-link to="/" class="d-inline-block auth-logo">
+                  <img src="@/assets/images/logo-dark.png" alt="" height="70">
+                </router-link>
+              </div>
+              <!-- <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p> -->
+            </div>
+          </BCol>
+        </BRow>
+
+        <BRow class="justify-content-center">
+          <BCol md="8" lg="6" xl="5">
+            <BCard no-body class="mt-4">
+              <BCardBody class="p-4 text-center">
+                <lottie colors="primary:#25a0e2,secondary:#00bd9d" :options="defaultOptions" :height="180"
+                  :width="180" />
+                <div class="mt-4 pt-2">
+                  <h5>Deslogueado</h5>
+                  <p class="text-muted">Gracias por usar <span class="fw-semibold">RauDoc</span></p>
+                  <div class="mt-4">
+                    <router-link to="/auth/signin-basic" class="btn btn-info w-100">Ingresar
+                    </router-link>
+                  </div>
+                </div>
+              </BCardBody>
+            </BCard>
+
+          </BCol>
+        </BRow>
+      </BContainer>
+    </div>
+
+    <footer class="footer">
+      <BContainer>
+        <BRow>
+          <BCol lg="12">
+            <div class="text-center">
+              <p class="mb-0 text-muted">&copy; {{ new Date().getFullYear() }} RauDoc. Powered by <i
+                  class="mdi mdi-heart text-danger"></i> by CooWeb</p>
+            </div>
+          </BCol>
+        </BRow>
+      </BContainer>
+    </footer>
+  </div>
+</template>
