74f1b42ea73f7f5d7a3ef8e7c8090f0d8e8bec97
Camilo López <camilolopez1506@gmail.com>
2024-04-04 19:59:09 -0500

    Feat: Rename list

---
+++ src/views/tickets/List copy.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Layout from "@/layouts/main.vue";
+import PageHeader from "@/components/page-header";
+import TableDocuments from "@/views/tickets/components/TableDocuments";
+import ClaimsCounter from "@/views/tickets/components/ClaimsCounter";
+
+</script>
+
+<template>
+    <Layout>
+        <div>
+            <PageHeader title="LISTA DE ENTRADAS" pageTitle="Radicados" />
+
+            <ClaimsCounter />
+
+            <TableDocuments />
+        </div>
+    </Layout>
+</template>
+
+<style scoped>
+.footer {
+    position: relative;
+    left: 0;
+    right: 0;
+}
+</style>
