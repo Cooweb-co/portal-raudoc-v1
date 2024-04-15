c1c8c5e807aea9d85170389f5a553197fc8607ed
AlejandroJPonce <alejandrojoseponceg@gmail.com>
2024-04-15 15:37:51 -0500

    feat: new snapshot

--- src/services/docservice/doc.service.js
+++ src/services/docservice/doc.service.js
@@ -1,121 +1,159 @@
 import { getFirebaseBackend } from "../../authUtils.js";
 import {
-    doc,
-    onSnapshot,
-    getDoc,
-    getDocs,
-    collection,
+  doc,
+  onSnapshot,
+  getDoc,
+  getDocs,
+  collection,
 } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const firestore = getFirebaseBackend().firestore;
 
 export const createClaimID = async (uid) => {
-    // eslint-disable-next-line no-useless-catch
-    try {
-        const docRef = await firestore
-            .collection("Companies")
-            .doc("BAQVERDE")
-            .collection("Claims")
-            .add({
-                createdAt: new Date(),
-                year: new Date().getFullYear(),
-                uid,
-                status: "DRAFT",
-            });
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const docRef = await firestore
+      .collection("Companies")
+      .doc("BAQVERDE")
+      .collection("Claims")
+      .add({
+        createdAt: new Date(),
+        year: new Date().getFullYear(),
+        uid,
+        status: "DRAFT",
+      });
 
-        return docRef.id;
-    } catch (error) {
-        throw error;
-    }
+    return docRef.id;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const onListenClaimData = async (claimId, companyId, callback) => {
-    // eslint-disable-next-line no-useless-catch
+  try {
     return onSnapshot(
-        doc(firestore, "Companies", companyId, "Claims", claimId),
-        (doc) => {
-            console.log("onListenClaimData::::OnSnapshot", doc.data());
-            callback(doc.data());
-        }
+      doc(firestore, "Companies", companyId, "Claims", claimId),
+      (doc) => {
+        console.log("onListenClaimData::::OnSnapshot", doc.data());
+        callback(doc.data());
+      }
     );
+  } catch (error) {
+    console.log("error: ", error);
+  }
+  // eslint-disable-next-line no-useless-catch
 };
 
 export const saveFile = async (pathDocument, urlPDF, fileName) => {
-    // eslint-disable-next-line no-useless-catch
-    try {
-        console.log("saveFile:::", pathDocument, urlPDF, fileName);
+  // eslint-disable-next-line no-useless-catch
+  try {
+    console.log("saveFile:::", pathDocument, urlPDF, fileName);
 
-        await firestore.doc(pathDocument).collection("Files").add({
-            name: fileName,
-            url: urlPDF,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        });
+    await firestore.doc(pathDocument).collection("Files").add({
+      name: fileName,
+      url: urlPDF,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
 
-        return true;
-    } catch (error) {
-        throw error;
-    }
+    return true;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getDocument = async (companyId, claimId) => {
-    try {
-        const docRef = doc(
-            firestore,
-            "Companies",
-            companyId,
-            "Claims",
-            claimId
-        );
-        const docSnap = await getDoc(docRef);
+  try {
+    const docRef = doc(firestore, "Companies", companyId, "Claims", claimId);
+    const docSnap = await getDoc(docRef);
 
-        if (docSnap.exists()) {
-            return docSnap.data();
-        } else {
-            console.log("No existe el document!");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error al obtener el documento:", error);
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      console.log("No existe el document!");
+      return null;
     }
+  } catch (error) {
+    console.error("Error al obtener el documento:", error);
+  }
 };
 
 export const getDocumentFilesUploads = async (companyId, claimId) => {
-    try {
-        const collectionRef = collection(
-            firestore,
-            "Companies",
-            companyId,
-            "Claims",
-            claimId,
-            "Files"
-        );
-        const docsSnap = await getDocs(collectionRef);
+  try {
+    const collectionRef = collection(
+      firestore,
+      "Companies",
+      companyId,
+      "Claims",
+      claimId,
+      "Files"
+    );
+    const docsSnap = await getDocs(collectionRef);
 
-        if (!docsSnap.empty) {
-            let documents = [];
-            docsSnap.forEach((doc) => {
-                documents.push(doc.data());
-            });
-            return documents;
-        } else {
-            console.log("No existen documentos en la colección!");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error al obtener los documentos:", error);
+    if (!docsSnap.empty) {
+      let documents = [];
+      docsSnap.forEach((doc) => {
+        documents.push(doc.data());
+      });
+      return documents;
+    } else {
+      console.log("No existen documentos en la colección!");
+      return null;
     }
+  } catch (error) {
+    console.error("Error al obtener los documentos:", error);
+  }
 };
 
 export async function openDocument(fileName, filePath) {
-    const storage = getStorage();
-    const storageRef = ref(storage, `${filePath}/${fileName}`);
+  const storage = getStorage();
+  const storageRef = ref(storage, `${filePath}/${fileName}`);
 
-    try {
-        const url = await getDownloadURL(storageRef);
-        window.open(url, "_blank");
-    } catch (error) {
-        console.error("Error al obtener URL del archivo:", error);
+  try {
+    const url = await getDownloadURL(storageRef);
+    window.open(url, "_blank");
+  } catch (error) {
+    console.error("Error al obtener URL del archivo:", error);
+  }
+}
+
+export async function getDocStatus(companyId, claimId) {
+  try {
+    const collectionRef = collection(
+      firestore,
+      "Companies",
+      companyId,
+      "Claims",
+      claimId,
+      "Files"
+    );
+
+    const docsSnap = await getDocs(collectionRef);
+
+    if (!docsSnap.empty) {
+      console.log('entre a la validacion');
+      let documents = [];
+      console.log('documentos vacios');
+      docsSnap.forEach((doc) => {
+        documents.push(doc.data());
+        console.log(doc.data());
+      });
+
+      onSnapshot(collectionRef, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log("Nuevo estado del documento:", doc.data());
+          let status = doc.data?.status
+          if (status === 'ERROR') {
+            return false
+          } else if (status === 'PROCESSED') {
+            return true
+          }
+        });
+      });
+
     }
+  } catch (error) {
+    console.error("Error al obtener los documentos:", error);
+  }
 }
--- src/views/admin-doc/create.vue
+++ src/views/admin-doc/create.vue
@@ -16,12 +16,18 @@ import { MESSAGE_REQUIRED, MESSAGE_EMAIL } from "../../constants/rules.ts";
 import Modal from "../modals/Modal.vue";
 import moment from 'moment'
 
+import {
+  onSnapshot,
+  collection,
+} from "firebase/firestore";
+
 //google places api imports
 import { Loader } from "@googlemaps/js-api-loader";
 
 import {
   createClaimID,
   onListenClaimData,
+  getDocStatus
 } from "../../services/docservice/doc.service";
 import axios from "axios";
 
@@ -109,56 +115,86 @@ export default {
 
     const v$ = useVuelidate(rules, form.value);
 
-    const startListening = () => {
-      idProccessAI = toast("Analizando documento con IA...", {
-        isLoading: true,
-        hideProgressBar: true,
-        closeButton: false,
-        closeOnClick: false,
+
+    const collectionRef = collection(
+      storage,
+      "Companies",
+      companyID.value,
+      "Claims",
+      companyID.value,
+      "Files"
+    );
+
+    onSnapshot(collectionRef, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log("Nuevo estado del documento:", doc.data());
+          let status = doc.data?.status
+          if (status === 'ERROR') {
+            return false
+          } else if (status === 'PROCESSED') {
+            return true
+          }
+        });
       });
 
-      unsubscribe = onListenClaimData(
-        documentID.value,
-        companyID.value,
-        (data) => {
-          claimData.value = data;
-          if (data.status == "DRAFT" && data.summary == null) {
-            loadingAI.value = true;
-            setTimeout(() => {
+    const startListening = () => {
+
+      try {
+        idProccessAI = toast("Analizando documento con IA...", {
+          isLoading: true,
+          hideProgressBar: true,
+          closeButton: false,
+          closeOnClick: false,
+        });
+
+        unsubscribe = onListenClaimData(
+          documentID.value,
+          companyID.value,
+          async (data) => {
+            let statusDoc = await getDocStatus(companyID.value, documentID.value)
+            console.log(statusDoc);
+            claimData.value = data;
+            if (data.status == "DRAFT" && data.summary == null) {
+              loadingAI.value = true;
+              setTimeout(() => {
+                toast.update(idProccessAI, {
+                  render: "Extrayendo información relevante...",
+                  type: toast.TYPE.INFO,
+                });
+              }, 3000);
+            }
+            if (data.summary) {
               toast.update(idProccessAI, {
-                render: "Extrayendo información relevante...",
-                type: toast.TYPE.INFO,
+                render: "Resumen realizado con exito!",
+                type: toast.TYPE.SUCCESS,
+                isLoading: false,
+                autoClose: 3000,
               });
-            }, 3000);
-          }
-          if (data.summary) {
-            toast.update(idProccessAI, {
-              render: "Resumen realizado con exito!",
-              type: toast.TYPE.SUCCESS,
-              isLoading: false,
-              autoClose: 3000,
-            });
-            loadingAI.value = false;
-          }
+              loadingAI.value = false;
+            }
 
-          if (data.status == "ERROR") {
-            toast.update(idProccessAI, {
-              render:
-                "Complete la información de asunto y resumen manualmente. Documento no valido para este proceso.",
-              type: toast.TYPE.WARNING,
-              isLoading: false,
-              autoClose: 1000,
-            });
-            loadingAI.value = false;
-          }
+            if (data.status === 'ERROR') {
+              toast.update(idProccessAI, {
+                render:
+                  "Complete la información de asunto y resumen manualmente. Documento no valido para este proceso.",
+                type: toast.TYPE.WARNING,
+                isLoading: false,
+                autoClose: 1000,
+              });
+              loadingAI.value = false;
+            }
 
-          instance.proxy.subject = data.subject ? data.subject : "";
-          instance.proxy.editorData = data.summary ? data.summary : "";
-          form.value.subject = data.subject ? data.subject : "";
-          form.value.description = data.summary ? data.summary : "";
-        }
-      );
-      return unsubscribe;
+            instance.proxy.subject = data.subject ? data.subject : "";
+            instance.proxy.editorData = data.summary ? data.summary : "";
+            form.value.subject = data.subject ? data.subject : "";
+            form.value.description = data.summary ? data.summary : "";
+          }
+        );
+        return unsubscribe;
+      } catch (error) {
+        console.log('error: ', error);
+      }
+
     };
 
     const drop = (e) => {
