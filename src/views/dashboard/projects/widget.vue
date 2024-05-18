667ee1344b07382da60431f5a333f796774d8df1
Camilo López <camilolopez1506@gmail.com>
2024-05-17 20:58:16 -0500

    feat: resize of count cards in dashboard

--- src/views/dashboard/projects/widget.vue
+++ src/views/dashboard/projects/widget.vue
@@ -1,10 +1,11 @@
 <script setup>
 import {
-  SendIcon,
-  AwardIcon,
-  ClockIcon,
-  CornerDownRightIcon,
+    SendIcon,
+    AwardIcon,
+    ClockIcon,
+    CornerDownRightIcon,
 } from "@zhuowenli/vue-feather-icons";
+import { CountTo } from "vue3-count-to";
 import { ref, onMounted } from "vue";
 import { state } from "@/state/modules/auth";
 import axios from "axios";
@@ -17,8 +18,7 @@ const projectsWidgets = ref();
 onMounted(async () => {
     try {
         loading.value = true;
-        const url =
-            `${process.env.VUE_APP_CF_BASE_URL}/GET_COUNT_CLAIMS_BY_USER`;
+        const url = `${process.env.VUE_APP_CF_BASE_URL}/GET_COUNT_CLAIMS_BY_USER`;
         const config = {
             params: {
                 uid: user.value.uid,
@@ -28,7 +28,7 @@ onMounted(async () => {
                 "Content-Type": "application/json",
             },
         };
-       
+
         const dataForCard = await axios.post(url, {}, config);
         if (dataForCard.data.length > 0) {
             loading.value = false;
@@ -37,19 +37,19 @@ onMounted(async () => {
             loading.value = false;
             projectsWidgets.value = [
                 {
-                    value: "Peticiones Recibidas",
+                    value: "Recibidas",
                     count: 0,
                 },
                 {
-                    value: "Peticiones Respondidas",
+                    value: "Respondidas",
                     count: 0,
                 },
                 {
-                    value: "Peticiones por responder",
+                    value: "por responder",
                     count: 0,
                 },
                 {
-                    value: "Documentos Procesados",
+                    value: "Procesados",
                     count: 0,
                 },
             ];
@@ -59,19 +59,19 @@ onMounted(async () => {
         loading.value = false;
         projectsWidgets.value = [
             {
-                value: "Peticiones Recibidas",
+                value: "Recibidas",
                 count: 0,
             },
             {
-                value: "Peticiones Respondidas",
+                value: "Respondidas",
                 count: 0,
             },
             {
-                value: "Peticiones por responder",
+                value: "Por responder",
                 count: 0,
             },
             {
-                value: "Documentos Procesados",
+                value: "Procesados",
                 count: 0,
             },
         ];
@@ -80,98 +80,119 @@ onMounted(async () => {
 </script>
 
 <template>
-  <pre>{{ props }}</pre>
-  <BRow v-if="loading">
-    <BCol no-body class="card-animate">
-      <a-skeleton :paragraph="{ rows: 2 }" active avatar />
-    </BCol>
-    <BCol no-body class="card-animate">
-      <a-skeleton :paragraph="{ rows: 2 }" active avatar />
-    </BCol>
-    <BCol no-body class="card-animate">
-      <a-skeleton :paragraph="{ rows: 2 }" active avatar />
-    </BCol>
-    <BCol no-body class="card-animate">
-      <a-skeleton :paragraph="{ rows: 2 }" active avatar />
-    </BCol>
-  </BRow>
+    <pre>{{ props }}</pre>
+    <BRow v-if="loading">
+        <BCol no-body class="card-animate">
+            <a-skeleton :paragraph="{ rows: 2 }" active avatar />
+        </BCol>
+        <BCol no-body class="card-animate">
+            <a-skeleton :paragraph="{ rows: 2 }" active avatar />
+        </BCol>
+        <BCol no-body class="card-animate">
+            <a-skeleton :paragraph="{ rows: 2 }" active avatar />
+        </BCol>
+        <BCol no-body class="card-animate">
+            <a-skeleton :paragraph="{ rows: 2 }" active avatar />
+        </BCol>
+    </BRow>
 
-  <div id="container">
-    <BCard v-for="(item, index) of projectsWidgets" :key="index" id="element" class="w-25 h-75">
-      <BCardBody class="p-1">
-        <div class="d-flex align-items-center">
-          <div class="avatar-sm flex-shrink-0">
-            <span
-              class="avatar-title rounded-2 fs-2"
-              :class="{
-                'bg-primary-subtle text-primary':
-                  item.value.toUpperCase() === 'primary' ||
-                  'DOCUMENTOS PROCESADOS',
-                'bg-warning-subtle text-warning':
-                  item.value.toUpperCase() === 'PETICIONES POR RESPONDER',
-                'bg-info-subtle text-info': item.value.toUpperCase() === 'info',
-              }"
-            >
-              <template
-                v-if="item.value.toUpperCase() == 'PETICIONES RESPONDIDAS'"
-              >
-                <SendIcon size="24" class="text-primary"></SendIcon>
-              </template>
+    <BRow>
+        <BCol
+            v-for="(item, index) of projectsWidgets"
+            :key="index"
+            id="element"
+            sm="3"
+        >
+            <BCard no-body class="card-animate">
+                <BCardBody>
+                    <div class="d-flex align-items-center px-1">
+                        <div class="avatar-sm flex-shrink-0">
+                            <span
+                                class="avatar-title rounded-2 fs-2"
+                                :class="{
+                                    'bg-primary-subtle text-primary':
+                                        item.value.toUpperCase() ===
+                                            'primary' || 'PROCESADOS',
+                                    'bg-warning-subtle text-warning':
+                                        item.value.toUpperCase() ===
+                                        'POR RESPONDER',
+                                    'bg-info-subtle text-info':
+                                        item.value.toUpperCase() === 'info',
+                                }"
+                            >
+                                <template
+                                    v-if="
+                                        item.value.toUpperCase() ==
+                                        'RESPONDIDAS'
+                                    "
+                                >
+                                    <SendIcon
+                                        size="24"
+                                        class="text-primary"
+                                    ></SendIcon>
+                                </template>
 
-              <template
-                v-if="item.value.toUpperCase() == 'PETICIONES RECIBIDAS'"
-              >
-                <CornerDownRightIcon
-                  size="24"
-                  class="text-primary"
-                ></CornerDownRightIcon>
-              </template>
+                                <template
+                                    v-if="
+                                        item.value.toUpperCase() == 'RECIBIDAS'
+                                    "
+                                >
+                                    <CornerDownRightIcon
+                                        size="24"
+                                        class="text-primary"
+                                    ></CornerDownRightIcon>
+                                </template>
 
-              <template
-                v-if="item.value.toUpperCase() == 'PETICIONES POR RESPONDER'"
-              >
-                <ClockIcon size="24" class="text-primary"></ClockIcon>
-              </template>
+                                <template
+                                    v-if="
+                                        item.value.toUpperCase() ==
+                                        'POR RESPONDER'
+                                    "
+                                >
+                                    <ClockIcon
+                                        size="24"
+                                        class="text-primary"
+                                    ></ClockIcon>
+                                </template>
 
-              <template
-                v-if="item.value.toUpperCase() == 'DOCUMENTOS PROCESADOS'"
-              >
-                <AwardIcon size="24" class="text-primary"></AwardIcon>
-              </template>
-            </span>
-          </div>
-          <div class="flex-grow-1 overflow-hidden ms-3">
-            <p class="text-uppercase fw-medium text-muted text-truncate mb-3">
-              {{ item.value }}
-            </p>
-            <div class="d-flex align-items-center mb-3">
-              <h4 class="fs-4 flex-grow-1 mb-0">
-                <span class="counter-value">{{ item.count }}</span>
-              </h4>
-            </div>
-          </div>
-        </div>
-      </BCardBody>
-    </BCard>
-  </div>
+                                <template
+                                    v-if="
+                                        item.value.toUpperCase() == 'PROCESADOS'
+                                    "
+                                >
+                                    <AwardIcon
+                                        size="24"
+                                        class="text-primary"
+                                    ></AwardIcon>
+                                </template>
+                            </span>
+                        </div>
+                        <div class="flex-grow-1 overflow-hidden ms-3">
+                            <p
+                                class="text-uppercase fw-medium text-muted text-truncate mb-3"
+                            >
+                                {{ item.value }}
+                            </p>
+                            <div class="d-flex align-items-center mb-3">
+                                <h4 class="fs-4 flex-grow-1 mb-0">
+                                    <count-to
+                                        :duration="1000"
+                                        :startVal="0"
+                                        :endVal="item.count"
+                                    ></count-to>
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </BCardBody>
+            </BCard>
+        </BCol>
+    </BRow>
 </template>
 
 <style>
-#container {
-  display: grid;
-  align-items: center;
-  grid-auto-rows: auto;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 10px;
-  overflow-x: auto;
-  padding-left: 0;
-  padding-right: 2em;
-  margin-right: 2em;
-  margin-left: 1em;
-}
-
 #element {
-  background-color: white;
-  min-width: 300px;
+    background-color: white;
+    min-width: 16em;
 }
 </style>
